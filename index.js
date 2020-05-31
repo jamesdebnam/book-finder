479a13048f06f8fd02dd020d6c53c9183a29f6cd
jamesdebnam <jamesdebnam@gmail.com>
2020-05-31 17:48:26 +0100

    Added select one book functionality

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const bookPredict = require("./components/bookPredictGet.js");
 
 const input = document.querySelectorAll("input");
-console.log(input);
+
 input.forEach((item) => item.addEventListener("change", displayBooks));
 
 async function displayBooks(e) {
+  //Grabs info from google books API
   let bookArray = await bookPredict.getBookArray(e.target.value);
+  let displayedBooks = Array.from(
+    document.querySelector(`.book-input__${e.target.id}`).children
+  );
+  if (displayedBooks.length > 0) {
+    displayedBooks.forEach((item) => {
+      item.remove();
+    });
+  }
 
   for (let i = 0; i < 3; i++) {
+    //Create all the necessary elements for each card
     let title = document.createElement("p");
     title.setAttribute("class", `title title-${i}`);
     title.innerHTML = bookArray[i].title;
@@ -17,15 +27,32 @@ async function displayBooks(e) {
     let image = document.createElement("img");
     image.setAttribute("src", `${bookArray[i].imageLinks.thumbnail}`);
     image.setAttribute("class", "cover");
-    let bookCard = document.createElement("div");
+
+    //Selects the ul element that the cards are going to be appended onto
     let inputParent = document.querySelector(`.book-input__${e.target.id}`);
+    let bookCard = document.createElement("li");
+
+    //Appends all the created elements into the li element and fits that into the DOM
     bookCard.appendChild(title);
     bookCard.appendChild(author);
     bookCard.appendChild(image);
     inputParent.appendChild(bookCard);
+
+    //Sets attributes twice - this is to get the CSS transition working
     bookCard.setAttribute("class", "book-card");
     setTimeout(() => {
-      bookCard.setAttribute("class", `book-card--active`);
+      bookCard.setAttribute("class", `book-card--active book-card__book${i}`);
     }, 10);
+    bookCard.addEventListener("click", selectBook);
   }
+}
+
+function selectBook(e) {
+  let bookList = e.target.parentElement.childNodes;
+  for (let i = 0; i < bookList.length; i++) {
+    if (bookList[i].className !== e.target.className) {
+      bookList[i].remove();
+    }
+  }
+  // e.target.setAttribute("class", `book-card--active book-card--selected`);
 }
